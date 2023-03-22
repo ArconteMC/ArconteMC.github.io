93606810f41d822786a2b9e59252cb9c00cedb7c
ArconteMC <alan_2011_borbor@hotmail.com>
2023-03-22 10:14:23 -0500

    listen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+
+
+const mainNav = document.getElementById('main-nav');
+const toggleMenu = document.getElementById('toggle-menu');
+
+toggleMenu.addEventListener('click', ()=> {
+    mainNav.classList.toggle('nav--show');
+});
+
+window.addEventListener('resize', ()=>{
+    if(mainNav.classList.contains('nav--show')){
+        mainNav.classList.remove('nav--show');
+    mariMenu.classList.remove('nav--show');('main-menu--show');
+    }
+});
+
+
+
+
 let listElements = document.querySelectorAll('.list__button--click');
 
 listElements.forEach(listElement => {
@@ -17,22 +36,3 @@ listElements.forEach(listElement => {
 });
 
 
-const $tiempo=document.querySelector('.tiempo'),
-$fecha= document.querySelector('.fecha');
-function Relojdigital(){
-let f=new Date(),
-dia= f.getDate(),
-mes= f.getMonth()+1,
-anio= f.getFullYear(),
-diaSemana=f.getDay();
-dia= ('0'+dia).slice(-2);
-mes=('0'+mes).slice(-2)
-let timeString= f.toLocaleTimeString();
-$tiempo.innerHTML=timeString;
-let semana=['DOMINGO','LUNES','MARTES','MIERCOLES','JUEVES','VIERNES','SABADO'];
-let showSemana= (semana[diaSemana])
-$fecha.innerHTML = `${showSemana} ${dia}-${mes}-${anio}`
-}
-setInterval(() =>{
-Relojdigital()
-},1000);
